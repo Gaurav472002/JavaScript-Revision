955e0637b0f5636c6dc472bc2b41b835d34fa972
Gaurav Chakraborty <sagarchakraborty472@gmail.com>
2025-11-03 12:37:59 +0530

    Implement generic greet function with call method
    Added a generic greet function that can be called with different user objects.

--- 09_OOPS_Call_Bind_getters_setters/13_Call.js
+++ 09_OOPS_Call_Bind_getters_setters/13_Call.js
@@ -23,3 +23,26 @@ let User2 ={
 // User2.greet(); // this will throw an error
 // We can borrow the function using the call function
 User.greet.call(User2);
+
+
+// Suppose now the same function is defined outside and now it is a generic function that is not limited to any object and can be access by any object
+
+ let greet =function(){
+        
+        console.log(`Hi ${this.name} ! Good Morning`);
+    }
+
+let User1 ={
+    name: "Gaurav",
+    age: 18,
+   
+}
+let User2 ={
+    name: "Saurav",
+    age: 18,
+   
+}
+
+greet.call(User1);
+greet.call(User2);
+
